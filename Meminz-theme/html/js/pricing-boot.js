cec853350885397241153f2f60edc2b59bbcab92
Ilija Poznic <ikapoz@gmail.com>
2015-03-09 15:12:03 +0100

    pricing page update

--- Meminz-theme/html/js/pricing-boot.js
+++ Meminz-theme/html/js/pricing-boot.js
@@ -2,22 +2,75 @@ require(["jquery", "knockout"], function ($, ko) {
     var hideDiv,
         VM = function () {
             var self = this,
-                saving = {
-                    monthToMonth: 0,
-                    monthly: 0.95,
-                    quarterly: 0.9,
-                    annual: 0.85
+                _init,
+                _calculated,
+                _saving = {
+                    monthToMonth: {
+                        period: 'monthToMonth',
+                        saving: 0
+                    },
+                    monthly: {
+                        period: 'monthly',
+                        saving: 0.95
+                    },
+                    quarterly: {
+                        period: 'quarterly',
+                        saving: 0.9
+                    },
+                    annual: {
+                        period: 'annual',
+                        saving: 0.85
+                    }
                 }
+            _init = function () {
+                var counts = [];
+                for (var i = 2; i < 100; i++) {
+                    counts.push(i);
+                }
+                self.advisorCount(counts);
+            };
             // public
             self.isAnnualAggrement = ko.observable(false);
+            self.isAccountAggregationSelected = ko.observable(false);
             self.selectedPeriod = ko.observable("monthToMonth");
+            self.advisorCount = ko.observableArray([]);
             // one advisor
             self.advisorCost = ko.observable("$180");
+            self.advisorAnnualCost = ko.observable("$2,160");
 
             // team
             self.teamCostPerAdvisor = ko.observable("$164");
+            self.teamAdvisorSelectedCount = ko.observable(2);
 
+            // calculated
+            _calculated = ko.computed(function () {
+                var isAnnualAggrement = self.isAnnualAggrement(),
+                    selectedPeriod = self.selectedPeriod(),
+                    isAccountAggregationSelected = self.isAccountAggregationSelected(),
+                    teamAdvisorSelectedCount = self.teamAdvisorSelectedCount(),
+                    realCost,
+                    totalBaseCost,
+                    annualCost,
+                    check;
+                if (selectedPeriod === _saving.quarterly.period || selectedPeriod === _saving.annual.period) {
+                    self.isAnnualAggrement(true);
+                    isAnnualAggrement = true;
+                } else {
+                    if (selectedPeriod === _saving.monthToMonth.period && isAnnualAggrement) {
+                        selectedPeriod = _saving.monthly.period;
+                    }
+                }
+                // do the calculation
+                // first for one advisor
+                totalBaseCost = 180;
+                realCost = totalBaseCost * _saving[selectedPeriod].saving;
+                annualCost = realCost * 12;
 
+                console.log("computed executed, selected period: " + selectedPeriod);
+            });
+
+            // initialization
+            _init();
         };
 
     // application start
@@ -28,7 +81,10 @@ require(["jquery", "knockout"], function ($, ko) {
     //window.onload = newLoad;
     $(document).ready(function () {
         var vm = new VM();
+
         hideDiv(); // hide loading spin
-        ko.applyBindings(vm, document);
+        $.each($('body'), function (index, element) {
+            ko.applyBindings(vm, element);
+        });
     });
 });
--- Meminz-theme/html/js/requirejs-config.js
+++ Meminz-theme/html/js/requirejs-config.js
@@ -1,6 +1,7 @@
 require.config({
+    baseUrl: 'js',
     paths: {
-        jquery: "jquery-2.1.3.min",
-        knockout: "knockout-3.3.0"
+        jquery: 'jquery-2.1.3.min',
+        knockout: 'knockout-3.3.0'
     }
 });
