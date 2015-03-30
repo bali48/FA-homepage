e6c2f92b0627f58bacdb8c50efb123d52c2c740b
Jasmina Fabijan <jf@jasminafabijan.com>
2015-03-30 19:02:36 +0200

    Update and small change

--- Meminz-theme/html/js/pricing-boot.js
+++ Meminz-theme/html/js/pricing-boot.js
@@ -81,7 +81,7 @@ require(["jquery", "knockout"], function ($, ko) {
         };
         // public
         self.isAnnualAggrement = ko.observable(false);
-        self.isAccountAggregationSelected = ko.observable(false);
+        self.isAccountAggregationSelected = ko.observable(true);
         self.selectedPeriod = ko.observable("monthToMonth");
         self.advisorCount = ko.observableArray([]);
         self.titlePeriod = ko.observable("MTM")
@@ -100,6 +100,9 @@ require(["jquery", "knockout"], function ($, ko) {
         self.officeAdvisorRealCost = ko.observable("$3,660");
         self.officeSaving = ko.observable("%15.28");
         self.officeAdvisorSelectedCount = ko.observable(2);
+        self.officeAccountsTotal = ko.observable(500);
+        self.officeAggregationTotal = ko.observable(1000);
+        self.officeDocumentVault = ko.observable(500);
 
         // calculated
         _calculated = ko.computed(function () {
@@ -190,6 +193,9 @@ require(["jquery", "knockout"], function ($, ko) {
                 self.officeCostPerAdvisor(currencyFormat(realCost));
                 self.officeSaving(percentFormat(saving));
             }
+            self.officeAccountsTotal(officeAdvisorSelectedCount * 250);
+            self.officeAggregationTotal(officeAdvisorSelectedCount * 500);
+            self.officeDocumentVault(officeAdvisorSelectedCount * 250);
             console.log("computed executed, selected period: " + selectedPeriod);
         });
 
