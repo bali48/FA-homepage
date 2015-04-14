1e60aa48b19e24155dfd52cf94235f91633e5c04
Jasmina Fabijan <jf@jasminafabijan.com>
2015-04-14 16:03:20 +0200

    Update and added video file

--- Meminz-theme/html/js/requirejs-config.js
+++ Meminz-theme/html/js/requirejs-config.js
@@ -2,6 +2,7 @@ require.config({
     baseUrl: 'js',
     paths: {
         jquery: 'jquery-2.1.3.min',
-        knockout: 'knockout-3.3.0'
+        knockout: 'knockout-3.3.0',
+        videojs: 'video-4.12.5'
     }
 });
