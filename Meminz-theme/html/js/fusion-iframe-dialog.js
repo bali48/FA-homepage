00494f90b1ed7278cb5098fa145bfb9400d927b2
Jasmina Fabijan <jf@jasminafabijan.com>
2016-01-29 10:09:19 +0100

    Update

--- Meminz-theme/html/js/fusion-iframe-dialog.js
+++ Meminz-theme/html/js/fusion-iframe-dialog.js
@@ -27,11 +27,11 @@ define(['knockout',
                     _template$ = $(template);
                     $('body').append(_template$);
                     _iframe$ = $('[data-fa-id="modal-iframe"]');
-                    _iframe$.each(function (index, element) {
-                        element.onload = function () {
-                            self.isBusy(false);
-                        };
-                    });
+                    //_iframe$.each(function (index, element) {
+                    //    element.onload = function () {
+                    //        self.isBusy(false);
+                    //    };
+                    //});
                     ko.cleanNode(_template$[0]); // clear previous binding
                     ko.applyBindings(self, _template$[0]);
                 };
@@ -40,7 +40,7 @@ define(['knockout',
                 self.isBusy = ko.observable(false);
                 self.title = ko.observable('');
                 self.show = function (src) {
-                    self.isBusy(true);
+                    //self.isBusy(true);
                     _iframe$.attr('src', src);
                     _template$.modal();
                     _template$.on('hide.bs.modal', function (e) {
